3cfcd3943d268770264e245f2122c16478c515c5
xhpx <932762865@qq.com>
2020-12-30 13:16:31 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -37,7 +37,7 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        'docs.deepin.com',
+        'docs.deepin.com'
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
 
